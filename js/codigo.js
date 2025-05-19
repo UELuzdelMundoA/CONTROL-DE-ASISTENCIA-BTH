5ad7241e2e23fd9c2e3cefc54aa9adaa3dd1c49d
UELuzdelMundoA <u.e.luzdelmundo@gmail.com>
2025-05-19 11:08:35 -0400

    mostrar tabla de asistencia y actualizar con firebase

--- js/codigo.js
+++ js/codigo.js
@@ -23,3 +23,39 @@ function activarModoAdministrador(){
         alert("Contraseña incorrecta");
     }
 }
+//Crear lista de alumnos
+function crearListaEstudiantesPorDefecto() {
+    const estudiantes = [];
+    for (let i = 1; i <= 45; i++) {
+        estudiantes.push(nombre: `Estudiante ${i}`,asistencia{})
+    }
+    return estudiantes;
+}
+//Mostrar años Disponibles
+function mostrarAnios(carrera) {
+    // document.getElementById(): método del DOM que selecciona un elemento HTML por su ID
+    const contenido = document.getElementById('contenido');
+
+    // .innerHTML: propiedad que permite reemplazar el contenido HTML de un elemento
+    // En este caso, se inserta HTML con tres tarjetas para seleccionar los años escolares en el div id contenido
+    contenido.innerHTML = `
+    <h2>${carrera}</h2>
+    <div class='cuadricula'>
+        <div class="tarjeta" onclick="mostrarTrimestres('${carrera}', '4to')">4to de Secundaria</div>
+        <div class="tarjeta" onclick="mostrarTrimestres('${carrera}', '5to')">5to de Secundaria</div>
+        <div class="tarjeta" onclick="mostrarTrimestres('${carrera}', '6to')">6to de Secundaria</div>
+    </div>`;
+}
+//Mostrar trimestres disponibles
+function mostrarTrimestres(carrera, anio) {
+    const contenido = document.getElementById('contenido');
+
+    // Muestra tres botones (tarjetas) para seleccionar los trimestres de un año específico 
+    contenido.innerHTML = `
+    <h2>${carrera} - ${anio}</h2>
+    <div class='cuadricula'>
+        <div class="tarjeta" onclick="mostrarTablaAsistencia('${carrera}', '${anio}', '1er Trimestre')">1er Trimestre</div>
+        <div class="tarjeta" onclick="mostrarTablaAsistencia('${carrera}', '${anio}', '2do Trimestre')">2do Trimestre</div>
+        <div class="tarjeta" onclick="mostrarTablaAsistencia('${carrera}', '${anio}', '3er Trimestre')">3er Trimestre</div>
+    </div>`;
+}
